0ec12e56782b324f6dcfed8cd967d142d3e32f92
Torh-21 <majarotoheeb@gmail.com>
2024-04-02 23:39:37 +0100

    Changes added to tasks

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,12 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-	  mode: "production",
-	  entry: {
-		      main: path.resolve(__dirname, "./js/dashboard_main.js"),
-		    },
-	  output: {
-		      path: path.resolve(__dirname, "public"),
-		      filename: "bundle.js",
-		    },
-};
+    mode: 'production',
+    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    performance: {
+    maxAssetSize: 1000000,
+  },
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|gif|png|jpeg|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      }
+    ]
+  }
+}
